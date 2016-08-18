0683da0f5d2b20730c357a4e00393cdda6b92fa2
Kenn Takara <kenn.takara@percona.com>
2016-08-17 17:43:29 -1000

    Update for 3-1-3 instead of 3-1-1 topology

--- lp1609790/pxc-garbd-file.sh
+++ lp1609790/pxc-garbd-file.sh
@@ -8,19 +8,19 @@
 #
 # This assumes that:
 #   Machine 1: segment1 (3 nodes)
-#   Machine 2: segment10 (1 node)
-#   Machine 3: segment0 (arbitrator)
+#   Machine 2: segment2 (3 node)
+#   Machine 3: segment10 (arbitrator)
 #
 # Procedure:
 #   Machine 1:  init_pxc1
 #               start_pxc1
-#   Machine 2:  init_pxc10
-#               start_pxc10
+#   Machine 2:  init_pxc2
+#               start_pxc2
 #   Machine 3:  init_arb
 #               start_arb
 #
 #               sudo arb disconnect 1
-#               sudo arb disconnect 10
+#               sudo arb disconnect 2
 #
 #               sudo arb connect 1
 #
@@ -32,13 +32,13 @@
 # check for config file parameter
 if (( "$#" != 4 )); then
   echo ""
-  echo "Usage:  pxc-config-file <config-file> <ipaddr-seg0> <ipaddr-seg1> <ipaddr-arb>"
+  echo "Usage:  pxc-config-file <config-file> <ipaddr-seg1> <ipaddr-seg2> <ipaddr-arb>"
   echo ""
   echo "Creates the following scripts:"
   echo "  init_pxc1  : Creates subdirectories and initializes the datadirs"
   echo "  start_pxc1 : Starts up a 3-node cluster (segment 1)"
-  echo "  init_pxc10 : Creates subdirectories and initializes the datadirs"
-  echo "  start_pxc10: Starts up a 1-node cluster (segment 10)"
+  echo "  init_pxc2  : Creates subdirectories and initializes the datadirs"
+  echo "  start_pxc2 : Starts up a 3-node cluster (segment 2)"
   echo "  stop_pxc   : Stops the cluster"
   echo "  init_arb   : Creates subdirectories"
   echo "  start_arb  : Starts up the arb"
@@ -54,8 +54,8 @@ BUILD=$(pwd)
 
 config_file_path="${1}"
 ipaddr1="${2}"
-ipaddr10="${3}"
-ipaddr0="${4}"
+ipaddr2="${3}"
+ipaddr10="${4}"
 
 if [[ ! -r "${config_file_path}" ]]; then
   echo "Cannot find the config file : '${config_file_path}'"
@@ -66,8 +66,8 @@ echo ""
 echo "Adding scripts:"
 echo "  init_pxc1  : Creates subdirectories and initializes the datadirs"
 echo "  start_pxc1 : Starts up a 3-node cluster (segment 1)"
-echo "  init_pxc10 : Creates subdirectories and initializes the datadirs"
-echo "  start_pxc10: Starts up a 1-node cluster (segment 10)"
+echo "  init_pxc2  : Creates subdirectories and initializes the datadirs"
+echo "  start_pxc2 : Starts up a 3-node cluster (segment 2)"
 echo "  stop_pxc   : Stops the cluster"
 echo "  init_arb   : "
 echo "  start_arb  : "
@@ -89,25 +89,38 @@ RBASE3=4300
 LADDR3="$ipaddr1:$(( RBASE3 + 30 ))"
 RADDR3="$ipaddr1:$(( RBASE3 + 20 ))"
 
-RBASE4=5000
-LADDR4="$ipaddr10:$(( RBASE4 + 30 ))"
-RADDR4="$ipaddr10:$(( RBASE4 + 20 ))"
+RBASE4=5100
+LADDR4="$ipaddr2:$(( RBASE4 + 30 ))"
+RADDR4="$ipaddr2:$(( RBASE4 + 20 ))"
+
+RBASE5=5200
+LADDR5="$ipaddr2:$(( RBASE5 + 30 ))"
+RADDR5="$ipaddr2:$(( RBASE5 + 20 ))"
+
+RBASE6=5300
+LADDR6="$ipaddr2:$(( RBASE6 + 30 ))"
+RADDR6="$ipaddr2:$(( RBASE6 + 20 ))"
+
 
 RBASE10=9000
-LADDR10="$ipaddr0:$(( RBASE10 + 30 ))"
-RADDR10="$ipaddr0:$(( RBASE10 + 20 ))"
+LADDR10="$ipaddr10:$(( RBASE10 + 30 ))"
+RADDR10="$ipaddr10:$(( RBASE10 + 20 ))"
 
 
 node1="${BUILD}/node1"
 node2="${BUILD}/node2"
 node3="${BUILD}/node3"
 node4="${BUILD}/node4"
+node5="${BUILD}/node5"
+node6="${BUILD}/node6"
 node10="${BUILD}/node10"
 
 innodb_tempdir1="${BUILD}/innodb_tempdir1"
 innodb_tempdir2="${BUILD}/innodb_tempdir2"
 innodb_tempdir3="${BUILD}/innodb_tempdir3"
 innodb_tempdir4="${BUILD}/innodb_tempdir4"
+innodb_tempdir5="${BUILD}/innodb_tempdir5"
+innodb_tempdir6="${BUILD}/innodb_tempdir6"
 
 
 #
@@ -134,25 +147,27 @@ echo "\${MID} --datadir=$node3  > ${BUILD}/startup_node3.err 2>&1 || exit 1;" >>
 echo -e "\n" >> ./init_pxc1
 
 #
-# Create the init_pxc10 script 
+# Create the init_pxc2 script
 #
-echo "echo 'Creating subdirectores'" > ./init_pxc10
-echo "mkdir -p $node4" >> ./init_pxc10
-echo "mkdir -p $innodb_tempdir4" >> ./init_pxc10
-echo "mkdir -p /tmp/node4" >> ./init_pxc10
+echo "echo 'Creating subdirectores'" > ./init_pxc2
+echo "mkdir -p $node4 $node5 $node6" >> ./init_pxc2
+echo "mkdir -p $innodb_tempdir4 $innodb_tempdir5 $innodb_tempdir6" >> ./init_pxc2
+echo "mkdir -p /tmp/node4 /tmp/node5 /tmp/node6" >> ./init_pxc2
 
-echo "echo 'Initializing datadirs'" >> ./init_pxc10
-echo "if [ \"$(${BUILD}/bin/mysqld-debug --version | grep -oe '5\.[567]' | head -n1)\" == \"5.7\" ]; then" >> ./init_pxc10
-echo "  MID=\"${BUILD}/bin/mysqld-debug --no-defaults --initialize-insecure --innodb-undo-tablespaces=2 --innodb_log_checksums=ON --basedir=${BUILD}\"" >> ./init_pxc10
-echo "elif [ \"$(${BUILD}/bin/mysqld-debug --version | grep -oe '5\.[567]' | head -n1)\" == \"5.6\" ]; then" >> ./init_pxc10
-echo "  MID=\"${BUILD}/scripts/mysql_install_db --no-defaults --basedir=${BUILD}\"" >> ./init_pxc10
-echo "fi" >> ./init_pxc10
+echo "echo 'Initializing datadirs'" >> ./init_pxc2
+echo "if [ \"$(${BUILD}/bin/mysqld-debug --version | grep -oe '5\.[567]' | head -n1)\" == \"5.7\" ]; then" >> ./init_pxc2
+echo "  MID=\"${BUILD}/bin/mysqld-debug --no-defaults --initialize-insecure --innodb-undo-tablespaces=2 --innodb_log_checksums=ON --basedir=${BUILD}\"" >> ./init_pxc2
+echo "elif [ \"$(${BUILD}/bin/mysqld-debug --version | grep -oe '5\.[567]' | head -n1)\" == \"5.6\" ]; then" >> ./init_pxc2
+echo "  MID=\"${BUILD}/scripts/mysql_install_db --no-defaults --basedir=${BUILD}\"" >> ./init_pxc2
+echo "fi" >> ./init_pxc2
 
-echo -e "\n" >> ./init_pxc10
+echo -e "\n" >> ./init_pxc2
 
-echo "\${MID} --datadir=$node4  > ${BUILD}/startup_node4.err 2>&1 || exit 1;" >> ./init_pxc10
+echo "\${MID} --datadir=$node4  > ${BUILD}/startup_node4.err 2>&1 || exit 1;" >> ./init_pxc2
+echo "\${MID} --datadir=$node5  > ${BUILD}/startup_node5.err 2>&1 || exit 1;" >> ./init_pxc2
+echo "\${MID} --datadir=$node6  > ${BUILD}/startup_node6.err 2>&1 || exit 1;" >> ./init_pxc2
 
-echo -e "\n" >> ./init_pxc10
+echo -e "\n" >> ./init_pxc2
 
 #
 # Create the init_arb script
@@ -183,7 +198,7 @@ echo "    --basedir=${BUILD} \$PXC_MYEXTRA \\" >> ./start_pxc1
 echo "    --wsrep-provider=${BUILD}/lib/libgalera_smm.so \\" >> ./start_pxc1
 echo "    --wsrep_cluster_address=gcomm://$LADDR1,$LADDR2,$LADDR3,$LADDR4 \\" >> ./start_pxc1
 echo "    --wsrep_sst_receive_address=$RADDR1 \\" >> ./start_pxc1
-echo "    --wsrep_node_incoming_address=$ADDR \\" >> ./start_pxc1
+echo "    --wsrep_node_incoming_address=$ipaddr1 \\" >> ./start_pxc1
 echo "    --wsrep_provider_options=\"evs.version=1;gmcast.listen_addr=tcp://$LADDR1;gmcast.segment=1\" \\" >> ./start_pxc1
 echo "    --wsrep-new-cluster  > $node1/node1.err 2>&1 &" >> ./start_pxc1
 
@@ -210,7 +225,7 @@ echo "    --basedir=${BUILD} \$PXC_MYEXTRA \\" >> ./start_pxc1
 echo "    --wsrep-provider=${BUILD}/lib/libgalera_smm.so \\" >> ./start_pxc1
 echo "    --wsrep_cluster_address=gcomm://$LADDR1,$LADDR2,$LADDR3,$LADDR4 \\" >> ./start_pxc1
 echo "    --wsrep_sst_receive_address=$RADDR2 \\" >> ./start_pxc1
-echo "    --wsrep_node_incoming_address=$ADDR \\" >> ./start_pxc1
+echo "    --wsrep_node_incoming_address=$ipaddr1 \\" >> ./start_pxc1
 echo "    --wsrep_provider_options=\"evs.version=1;gmcast.listen_addr=tcp://$LADDR2;gmcast.segment=1\" \\" >> ./start_pxc1
 echo "    > $node2/node2.err 2>&1 &" >> ./start_pxc1
 
@@ -237,7 +252,7 @@ echo "    --basedir=${BUILD} \$PXC_MYEXTRA \\" >> ./start_pxc1
 echo "    --wsrep-provider=${BUILD}/lib/libgalera_smm.so \\" >> ./start_pxc1
 echo "    --wsrep_cluster_address=gcomm://$LADDR1,$LADDR2,$LADDR3,$LADDR4 \\" >> ./start_pxc1
 echo "    --wsrep_sst_receive_address=$RADDR3 \\" >> ./start_pxc1
-echo "    --wsrep_node_incoming_address=$ADDR \\" >> ./start_pxc1
+echo "    --wsrep_node_incoming_address=$ipaddr1 \\" >> ./start_pxc1
 echo "    --wsrep_provider_options=\"evs.version=1;gmcast.listen_addr=tcp://$LADDR3;gmcast.segment=1\" \\" >> ./start_pxc1
 echo "    > $node3/node3.err 2>&1 &" >> ./start_pxc1
 
@@ -253,38 +268,89 @@ echo -e "\n\n" >> ./start_pxc1
 
 
 #
-# Creating start_pxc10
+# Creating start_pxc2
 #
-echo "PXC_MYEXTRA=\"\"" > ./start_pxc10
-echo "PXC_START_TIMEOUT=30"  >> ./start_pxc10
-echo -e "\n" >> ./start_pxc10
-echo "echo 'Starting PXC nodes..'" >> ./start_pxc10
-echo -e "\n" >> ./start_pxc10
+echo "PXC_MYEXTRA=\"\"" > ./start_pxc2
+echo "PXC_START_TIMEOUT=30"  >> ./start_pxc2
+echo -e "\n" >> ./start_pxc2
+echo "echo 'Starting PXC nodes..'" >> ./start_pxc2
+echo -e "\n" >> ./start_pxc2
 
 #
 # Starting node 4
 #
-echo "echo 'Starting node 4..'" >> ./start_pxc10
+echo "echo 'Starting node 4..'" >> ./start_pxc2
 
-echo "${BUILD}/bin/mysqld-debug --defaults-file="${config_file_path}" --defaults-group-suffix=.4 \\" >> ./start_pxc10
-echo "    --port=$RBASE4 \\" >> ./start_pxc10
-echo "    --basedir=${BUILD} \$PXC_MYEXTRA \\" >> ./start_pxc10
-echo "    --wsrep-provider=${BUILD}/lib/libgalera_smm.so \\" >> ./start_pxc10
-echo "    --wsrep_cluster_address=gcomm://$LADDR1,$LADDR2,$LADDR3,$LADDR4 \\" >> ./start_pxc10
-echo "    --wsrep_sst_receive_address=$RADDR4 \\" >> ./start_pxc10
-echo "    --wsrep_node_incoming_address=$ADDR \\" >> ./start_pxc10
-echo "    --wsrep_provider_options=\"evs.version=1;gmcast.listen_addr=tcp://$LADDR4;gmcast.segment=10\" \\" >> ./start_pxc10
-echo "    > $node4/node4.err 2>&1 &" >> ./start_pxc10
+echo "${BUILD}/bin/mysqld-debug --defaults-file="${config_file_path}" --defaults-group-suffix=.4 \\" >> ./start_pxc2
+echo "    --port=$RBASE4 \\" >> ./start_pxc2
+echo "    --basedir=${BUILD} \$PXC_MYEXTRA \\" >> ./start_pxc2
+echo "    --wsrep-provider=${BUILD}/lib/libgalera_smm.so \\" >> ./start_pxc2
+echo "    --wsrep_cluster_address=gcomm://$LADDR1,$LADDR2,$LADDR3,$LADDR4 \\" >> ./start_pxc2
+echo "    --wsrep_sst_receive_address=$RADDR4 \\" >> ./start_pxc2
+echo "    --wsrep_node_incoming_address=$ipaddr2 \\" >> ./start_pxc2
+echo "    --wsrep_provider_options=\"evs.version=1;gmcast.listen_addr=tcp://$LADDR4;gmcast.segment=2\" \\" >> ./start_pxc2
+echo "    > $node4/node4.err 2>&1 &" >> ./start_pxc2
 
-echo -e "\n" >> ./start_pxc10
+echo -e "\n" >> ./start_pxc2
 
-echo "for X in \$( seq 0 \$PXC_START_TIMEOUT ); do" >> ./start_pxc10
-echo "  sleep 1" >> ./start_pxc10
-echo "  if ${BUILD}/bin/mysqladmin -uroot -S$node4/socket.sock ping > /dev/null 2>&1; then" >> ./start_pxc10
-echo "    break" >> ./start_pxc10
-echo "  fi" >> ./start_pxc10
-echo "done" >> ./start_pxc10
-echo -e "\n\n" >> ./start_pxc10
+echo "for X in \$( seq 0 \$PXC_START_TIMEOUT ); do" >> ./start_pxc2
+echo "  sleep 1" >> ./start_pxc2
+echo "  if ${BUILD}/bin/mysqladmin -uroot -S$node4/socket.sock ping > /dev/null 2>&1; then" >> ./start_pxc2
+echo "    break" >> ./start_pxc2
+echo "  fi" >> ./start_pxc2
+echo "done" >> ./start_pxc2
+echo -e "\n\n" >> ./start_pxc2
+
+#
+# Starting node 5
+#
+echo "echo 'Starting node 5..'" >> ./start_pxc2
+
+echo "${BUILD}/bin/mysqld-debug --defaults-file="${config_file_path}" --defaults-group-suffix=.5 \\" >> ./start_pxc2
+echo "    --port=$RBASE5 \\" >> ./start_pxc2
+echo "    --basedir=${BUILD} \$PXC_MYEXTRA \\" >> ./start_pxc2
+echo "    --wsrep-provider=${BUILD}/lib/libgalera_smm.so \\" >> ./start_pxc2
+echo "    --wsrep_cluster_address=gcomm://$LADDR1,$LADDR2,$LADDR3,$LADDR4 \\" >> ./start_pxc2
+echo "    --wsrep_sst_receive_address=$RADDR5 \\" >> ./start_pxc2
+echo "    --wsrep_node_incoming_address=$ipaddr2 \\" >> ./start_pxc2
+echo "    --wsrep_provider_options=\"evs.version=1;gmcast.listen_addr=tcp://$LADDR5;gmcast.segment=2\" \\" >> ./start_pxc2
+echo "    > $node5/node5.err 2>&1 &" >> ./start_pxc2
+
+echo -e "\n" >> ./start_pxc2
+
+echo "for X in \$( seq 0 \$PXC_START_TIMEOUT ); do" >> ./start_pxc2
+echo "  sleep 1" >> ./start_pxc2
+echo "  if ${BUILD}/bin/mysqladmin -uroot -S$node5/socket.sock ping > /dev/null 2>&1; then" >> ./start_pxc2
+echo "    break" >> ./start_pxc2
+echo "  fi" >> ./start_pxc2
+echo "done" >> ./start_pxc2
+echo -e "\n\n" >> ./start_pxc2
+
+#
+# Starting node 6
+#
+echo "echo 'Starting node 6..'" >> ./start_pxc2
+
+echo "${BUILD}/bin/mysqld-debug --defaults-file="${config_file_path}" --defaults-group-suffix=.6 \\" >> ./start_pxc2
+echo "    --port=$RBASE6 \\" >> ./start_pxc2
+echo "    --basedir=${BUILD} \$PXC_MYEXTRA \\" >> ./start_pxc2
+echo "    --wsrep-provider=${BUILD}/lib/libgalera_smm.so \\" >> ./start_pxc2
+echo "    --wsrep_cluster_address=gcomm://$LADDR1,$LADDR2,$LADDR3,$LADDR4 \\" >> ./start_pxc2
+echo "    --wsrep_sst_receive_address=$RADDR6 \\" >> ./start_pxc2
+echo "    --wsrep_node_incoming_address=$ipaddr2 \\" >> ./start_pxc2
+echo "    --wsrep_provider_options=\"evs.version=1;gmcast.listen_addr=tcp://$LADDR6;gmcast.segment=2\" \\" >> ./start_pxc2
+echo "    > $node6/node6.err 2>&1 &" >> ./start_pxc2
+
+echo -e "\n" >> ./start_pxc2
+
+echo "for X in \$( seq 0 \$PXC_START_TIMEOUT ); do" >> ./start_pxc2
+echo "  sleep 1" >> ./start_pxc2
+echo "  if ${BUILD}/bin/mysqladmin -uroot -S$node6/socket.sock ping > /dev/null 2>&1; then" >> ./start_pxc2
+echo "    break" >> ./start_pxc2
+echo "  fi" >> ./start_pxc2
+echo "done" >> ./start_pxc2
+echo -e "\n\n" >> ./start_pxc2
+
 
 #
 # Creating start_arb
@@ -294,7 +360,7 @@ echo -e "\n\n" >> ./start_pxc10
 echo "" > ./start_arb
 echo "${BUILD}/bin/garbd --name=arb --group=my_cluster \\" >> ./start_arb
 echo "    --address=gcomm://$LADDR1,$LADDR2,$LADDR3,$LADDR4 \\" >> ./start_arb
-echo "    --options=\"evs.version=1;gmcast.listen_addr=tcp://$LADDR10;gmcast.segment=0\" \\" >> ./start_arb
+echo "    --options=\"evs.version=1;gmcast.listen_addr=tcp://$LADDR10;gmcast.segment=10\" \\" >> ./start_arb
 echo "    --log=$node10/node10.err \\" >> ./start_arb
 echo "    > $node10/node10.err 2>&1 &" >> ./start_arb
 
@@ -302,6 +368,14 @@ echo "    > $node10/node10.err 2>&1 &" >> ./start_arb
 # Creating stop_pxc
 #
 echo "" > ./stop_pxc
+echo "if [[ -r $node6/socket.sock ]]; then" >> ./stop_pxc
+echo "  ${BUILD}/bin/mysqladmin -uroot -S$node6/socket.sock shutdown" >> ./stop_pxc
+echo "  echo 'Server on socket $node6/socket.sock with datadir ${BUILD}/node6 halted'" >> ./stop_pxc
+echo "fi" >> ./stop_pxc
+echo "if [[ -r $node5/socket.sock ]]; then" >> ./stop_pxc
+echo "  ${BUILD}/bin/mysqladmin -uroot -S$node5/socket.sock shutdown" >> ./stop_pxc
+echo "  echo 'Server on socket $node5/socket.sock with datadir ${BUILD}/node5 halted'" >> ./stop_pxc
+echo "fi" >> ./stop_pxc
 echo "if [[ -r $node4/socket.sock ]]; then" >> ./stop_pxc
 echo "  ${BUILD}/bin/mysqladmin -uroot -S$node4/socket.sock shutdown" >> ./stop_pxc
 echo "  echo 'Server on socket $node4/socket.sock with datadir ${BUILD}/node4 halted'" >> ./stop_pxc
@@ -333,26 +407,46 @@ echo "if [ -d $BUILD/node1.PREV ]; then rm -rf $BUILD/node1.PREV; fi;mv $BUILD/n
 echo "if [ -d $BUILD/node2.PREV ]; then rm -rf $BUILD/node2.PREV; fi;mv $BUILD/node2 $BUILD/node2.PREV" >> ./wipe
 echo "if [ -d $BUILD/node3.PREV ]; then rm -rf $BUILD/node3.PREV; fi;mv $BUILD/node3 $BUILD/node3.PREV" >> ./wipe
 echo "if [ -d $BUILD/node4.PREV ]; then rm -rf $BUILD/node4.PREV; fi;mv $BUILD/node4 $BUILD/node4.PREV" >> ./wipe
+echo "if [ -d $BUILD/node5.PREV ]; then rm -rf $BUILD/node5.PREV; fi;mv $BUILD/node5 $BUILD/node5.PREV" >> ./wipe
+echo "if [ -d $BUILD/node6.PREV ]; then rm -rf $BUILD/node6.PREV; fi;mv $BUILD/node6 $BUILD/node6.PREV" >> ./wipe
 echo "if [ -d $BUILD/node10.PREV ]; then rm -rf $BUILD/node10.PREV; fi;mv $BUILD/node10 $BUILD/node10.PREV" >> ./wipe
 
 echo "rm -rf ${innodb_tempdir1}" >> ./wipe
 echo "rm -rf ${innodb_tempdir2}" >> ./wipe
 echo "rm -rf ${innodb_tempdir3}" >> ./wipe
 echo "rm -rf ${innodb_tempdir4}" >> ./wipe
+echo "rm -rf ${innodb_tempdir5}" >> ./wipe
+echo "rm -rf ${innodb_tempdir6}" >> ./wipe
 
 echo "rm -rf /tmp/node1" >> ./wipe
 echo "rm -rf /tmp/node2" >> ./wipe
 echo "rm -rf /tmp/node3" >> ./wipe
 echo "rm -rf /tmp/node4" >> ./wipe
+echo "rm -rf /tmp/node5" >> ./wipe
+echo "rm -rf /tmp/node6" >> ./wipe
 
 #
 # Creating command-line scripts
 #
-echo "" > ./node_cl
+echo "#! /bin/bash" > ./node_cl
+echo "" >> ./node_cl
 echo "if (( \"\$#\" != 1 )); then" >> ./node_cl
 echo "  echo \"Usage: node_cl <node_number>\"" >> ./node_cl
+echo "  exit 1"
 echo "fi" >> ./node_cl
-echo "$BUILD/bin/mysql -A -uroot -S$BUILD/node\$1/socket.sock" >> ./node_cl
+echo "" >> ./node_cl
+echo "addresses=( '0' $ipaddr1 $ipaddr1 $ipadr1 $ipaddr2 $ipaddr2 $ipaddr2 )" >> ./node_cl
+echo "ports=('0' $RBASE1 $RBASE2 $RBASE3 $RBASE4 $RBASE5 $RBASE6 )" >> ./node_cl
+echo "if (( \$1 > \${#ports[@]} )); then" >> ./node_cl
+echo "  echo 'Invalid server number: \$1'" >> ./node_cl
+echo "  exit 1" >> ./node_cl
+echo "fi" >> ./node_cl
+echo "" >> ./node_cl
+echo "if [[ \${ports[\$1]} == '0' ]]; then" >> ./node_cl
+echo "  echo 'Invalid server number: \$1'" >> ./node_cl
+echo "  exit 1" >> ./node_cl
+echo "fi" >> ./node_cl
+echo "$BUILD/bin/mysql -A -h\${addresses[\$1]} -P\${ports[\$1]} -uroot " >> ./node_cl
 
 
 echo "#! /bin/bash" > ./arb
@@ -375,10 +469,10 @@ echo "" >> ./arb
 
 echo "if [ \"\$2\" -eq 1 ]; then" >> ./arb
 echo "  addr=\"$ipaddr1\"" >> ./arb
-echo "elif [ \"\$2\" -eq 10 ]; then" >> ./arb
-echo "  addr=\"$ipaddr10\"" >> ./arb
+echo "elif [ \"\$2\" -eq 2 ]; then" >> ./arb
+echo "  addr=\"$ipaddr2\"" >> ./arb
 echo "else" >> ./arb
-echo "  echo \"Only segments 1 and 10 are allowed : '\$2'\"" >> ./arb
+echo "  echo \"Only segments 1 and 2 are allowed : '\$2'\"" >> ./arb
 echo "  exit 1" >> ./arb
 echo "fi" >> ./arb
 
@@ -387,6 +481,6 @@ echo "iptables \$op OUTPUT -d \$addr -j DROP" >> ./arb
 
 echo "" >> ./arb
 
-chmod +x ./init_pxc1 ./init_pxc10 ./start_pxc1 ./start_pxc10 ./stop_pxc ./node_cl ./wipe
+chmod +x ./init_pxc1 ./init_pxc2 ./start_pxc1 ./start_pxc2 ./stop_pxc ./node_cl ./wipe
 chmod +x ./init_arb ./start_arb ./arb ./stop_arb
 
